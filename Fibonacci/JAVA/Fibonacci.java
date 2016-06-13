9c77f5bdd1b26b8f5cc77146485d933cb2464c41
PointSeven <mfisher@foxmail.com>
2016-06-13 20:10:23 +0800

    更新到第二次测试

--- Fibonacci/JAVA/Fibonacci.java
+++ Fibonacci/JAVA/Fibonacci.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Fibonacci {
 
-    public static void F(long *f, int n) {
+    static int[] fibarr = new int[1000];
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(Fib(n));
+    }
+
+    /*
+    public static int Fib (int n) {
         int f0, f1, f2;
-        f0 = f1 = 1;
+        f0 = f1 = f2 = 1;
 
         for (int i = 2; i <= n; i++) {
             f2 = f0 + f1;
@@ -10,6 +22,17 @@ public class Fibonacci {
             f1 = f2;
         }
 
-        System.out.printf("f[%d] = %d\n" , n, f2);
+        return f2;
+    }
+    */
+
+    public static int Fib (int n) {
+        int t = 0;
+
+        if (fibarr[n] != 0) return fibarr[n];
+        if (n == 0 || n == 1) t = 1;
+        if (n > 1) t = Fib(n - 1) + Fib(n - 2);
+
+        return fibarr[n] = t;
     }
 }
