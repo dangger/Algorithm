f6e3e30b7635880353834906c107b58661df1c22
dangge <dangger@users.noreply.github.com>
2016-06-15 09:13:10 +0800

    Update BSearch.java

--- BSearch/JAVA/BSearch.java
+++ BSearch/JAVA/BSearch.java
@@ -4,7 +4,6 @@
  * */
 
 import java.util.Scanner;
-//import java.util.Arrays;
 
 public class BSearch {
 
@@ -17,41 +16,16 @@ public class BSearch {
             num[i] = sc.nextInt();
         }
 
-        /*
-        System.out.print("x = ");
-        */
+
         int x = sc.nextInt();
 
-        /*
-        System.out.print("num[] = " + num[0]);
-        for (int i = 1; i < n; i++) {
-            System.out.print(", " + num[i]);
-        }
-        System.out.println();
-        */
- 
-        //Arrays.sort(num, 0, n);
         Sort(num, 0, n);
 
-        /*
-        System.out.print("num[] = " + num[0]);
-        for (int i = 1; i < n; i++) {
-            System.out.print(", " + num[i]);
-        }
-        System.out.println();
-        */
 
-        /*
-        System.out.println("num[" + BSearch(num, x, 0, n - 1)
-                + "] is " + x);
-        System.out.println("num[" + BSearch(num, x, n)
-                + "] is " + x);
-        */
 
         //递归调用
         System.out.println(BSearch(num, x, 0, n - 1));
-        //非递归调用
-        //System.out.println(BSearch(num, x, n));
+
     }
 
     //递归实现
@@ -71,26 +45,6 @@ public class BSearch {
         }
     }
 
-    //非递归实现
-    /*
-    public static int BSearch (int[] num, int x, int n) {
-        int mid;
-        int low = 0, high = n - 1;
-
-        while (high >= low) {
-            mid = (low + high) / 2;
-            if (x == num[mid]) {
-                return mid;
-            } else if (x < num[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-    */
 
     public static void Sort(int[] num, int left, int right) {
         for (int i = left; i < right; i++) {
